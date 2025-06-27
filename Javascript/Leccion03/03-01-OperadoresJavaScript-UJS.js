d4ed86e704e5290e1213481c9280794f2b7868e5
Jairo Ubilla <jwu.monk@gmail.com>
2025-06-26 21:26:40 -0300

    Agregando la clase9 de javascript al repositorio

--- Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,41 @@ if( dentroRago >= valMin && dentroRago <= valMax){
 else{
     console.log("Esta fuera del rango establecido")
 }
+
+// Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+}
+else{
+    console.log("El padre No puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2)
+let numero = 12;
+resultado2 = numero % 2 == 0 ? "Es un numero PAR" : "Es un numero IMPAR";
+console.log(resultado2)
+
+//Convertir String a Number
+let miNumero = "21";//Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);//Esta es una funcion
+console.log(typeof edad2);
+//Funcion isNan
+if(isNaN(edad2)){ //No es un numero = is Not a Number(devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo numeros")
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar");
+    }
+    else{
+        console.log("Muy joven para votar");
+    }
+}
+
+//Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar";
+console.log(resultado3);
