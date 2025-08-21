5dc36942e61fc2c21bc20db35b0c04c330458342
jairoubilla <jwu.monk@gmail.com>
2025-08-20 22:20:00 -0300

    ingresando las clases de Java con break, continue y labels

--- Java/Leccion02/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ Java/Leccion02/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -22,5 +22,30 @@ public class EjercicioWhile01 {
         for(var contando = 0; contando < 7; contando++){
             System.out.println("contando = " + contando);
         }
+        
+        //Palabra reservada break
+        for(var contandos = 0; contandos < 7; contandos++){
+            if(contandos % 2 == 0){
+            System.out.println("contandos = " + contandos);
+            break;
+            }
+        }
+        
+        //Palabra reservada continue
+        for(var num = 0; num < 7; num++){
+            if(num % 2 != 0){
+                continue; //Vamos a la siguiente iteracion
+            }
+            System.out.println("num = " + num);
+        }
+        
+        //Etiqueta Labels
+        inicio:
+        for(var etiqueta = 0; etiqueta < 7; etiqueta++){
+            if(etiqueta %  2 != 0){
+                continue inicio;
+            }
+            System.out.println("etiqueta = " + etiqueta);
+        }
     }
 }
