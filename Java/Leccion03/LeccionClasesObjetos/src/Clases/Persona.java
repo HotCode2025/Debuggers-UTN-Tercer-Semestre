1aba2fd0ca5c137aba5c31f4c49d06b8409f26c1
jairoubilla <jwu.monk@gmail.com>
2025-10-08 08:29:50 -0300

    Commiteamos leccion 3 clases y objetos en java

--- Java/Leccion03/LeccionClasesObjetos/src/Clases/Persona.java
+++ Java/Leccion03/LeccionClasesObjetos/src/Clases/Persona.java
@@ -3,6 +3,12 @@ package Clases;
 
 public class Persona {
     // Atributos de la clase (Caracteristicas)
-    string nombre;
-    string apellido;
+    String nombre;
+    String apellido;
+    
+    // Métodos de la clase (Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
