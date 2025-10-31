418c2a56ac45ef2eb6565a69713a98b016cbd90a
jairoubilla <jwu.monk@gmail.com>
2025-10-30 23:18:08 -0300

    Subiendo clases de Java Leccion05

--- Java/Leccion05/Encapsulamiento/src/dominio/Persona.java
+++ Java/Leccion05/Encapsulamiento/src/dominio/Persona.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//7.6 Encapsulamiento Parte 1
 package dominio;
 
-/**
- *
- * @author jwumo
- */
 public class Persona {
+
+    //Atributos
+    private String nombre;
+    private double sueldo;
+    private boolean eliminado;
+
+    //Constructor
+    public Persona(String nombre, double sueldo, boolean eliminado) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.eliminado = eliminado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
     
+    //8.3 Método toString
+
+    public String toString() { //Comvierte en una cadena cada atributo
+        return "Persona [ nombre: " + this.nombre+
+                ", sueldo: " + this.sueldo+
+                ", eliminado: "+this.eliminado+" ]";
+    }
 }
