26480e2d3698f593b6276828944d3e06ad42af6f
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2026-05-15 18:58:28 -0300

    Python - Clase 01

---
+++ Python/Leccion01/1.5 Bloques else y finally al manejar excepciones.py
@@ -0,0 +1,26 @@
+resultado = None
+
+try:
+    a = int(input('Digite el primer numero: '))
+    b = int(input('Digite el segundo numero: '))
+
+    resultado = a / b
+
+except TypeError as e:
+    print(f'TypeError: Ocurrio un error: {type(e)}')
+
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError: Ocurrio un error: {type(e)}')
+
+except Exception as e:
+    print(f'Exception - Ocurrio un error: {type(e)}')
+
+else:
+    print('No se arrojo ninguna excepción')
+
+finally:
+    print('Ejecución del bloque finally')
+
+
+print(f'El resultado es: {resultado}')
+print('Seguimos ...')
