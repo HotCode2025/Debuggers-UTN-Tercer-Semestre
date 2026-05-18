ef4a51fbbb81666f79adc845e05e84c0924b012c
Celeste Dominguez <dominguezmceleste@gmail.com>
2026-05-17 23:25:30 -0300

    1.3 Procesar clases de exception más específicas

---
+++ Python/Leccion01/1.3 Procesar clases de exception más específicas.py
@@ -0,0 +1,12 @@
+resultado = None
+a = 7
+b = 0
+try:
+    resultado = a / b # modificamos
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
+
+    print(f'El resultado es: {resultado}')
+    print('seguimos...')
