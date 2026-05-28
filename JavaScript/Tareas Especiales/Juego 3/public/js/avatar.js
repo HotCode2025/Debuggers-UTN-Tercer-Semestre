5785b2b51e222675fc95fc3772a064868e00e5d7
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2026-05-27 22:03:47 -0300

    Tarea 27-05 - Juego Avatar
    Selección automática enemigo

--- JavaScript/Tareas Especiales/Juego 3/public/js/avatar.js
+++ JavaScript/Tareas Especiales/Juego 3/public/js/avatar.js
@@ -1,6 +1,11 @@
+function iniciarJuego() {
+    let botonPersonajeJugador = document.getElementById('boton-personaje');
+    botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
+}
+
 function seleccionarPersonajeJugador() {
-    let opcionesPersonajes = document.getElementsByName('personaje');
     let personajeSeleccionado = "";
+    let spanPersonajeJugador = document.getElementById('personaje-jugador');
 
     // uso de condicionales if-else 
     if (document.getElementById('zuko').checked) {
@@ -14,6 +19,7 @@ function seleccionarPersonajeJugador() {
     }
 
     // uso de bucle for
+    let opcionesPersonajes = document.getElementsByName('personaje');
     for (let i = 0; i < opcionesPersonajes.length; i++) {
         if (opcionesPersonajes[i].checked) {
             personajeSeleccionado = opcionesPersonajes[i].id;
@@ -23,11 +29,35 @@ function seleccionarPersonajeJugador() {
 
     // muestro el personaje seleccionado
     if (personajeSeleccionado !== "") {
-        alert('SELECCIONASTE TU PERSONAJE: ' + personajeSeleccionado.toUpperCase());
+        //alert('SELECCIONASTE TU PERSONAJE: ' + personajeSeleccionado.toUpperCase());
+        spanPersonajeJugador.innerHTML = personajeSeleccionado.toUpperCase();
+
+        seleccionarPersonajeEnemigo(personajeSeleccionado);
+
     } else {
         alert('POR FAVOR, SELECCIONA UN PERSONAJE ANTES DE CONTINUAR.');
     }
 }
 
-let botonPersonajeJugador = document.getElementById('boton-personaje');
-botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
+function seleccionarPersonajeEnemigo(personajeJugador) {
+    let spanPersonajeEnemigo = document.getElementById('personaje-enemigo');
+    let personajeEnemigo = "";
+
+    do {
+        let numeroAleatorio = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+
+        if (numeroAleatorio === 1) {
+            personajeEnemigo = "zuko";
+        } else if (numeroAleatorio === 2) {
+            personajeEnemigo = "katara";
+        } else if (numeroAleatorio === 3) {
+            personajeEnemigo = "aang";
+        } else if (numeroAleatorio === 4) {
+            personajeEnemigo = "toph";
+        }
+    } while (personajeEnemigo == personajeJugador);
+
+    spanPersonajeEnemigo.innerHTML = personajeEnemigo.toUpperCase();
+}
+
+window.addEventListener('load', iniciarJuego);
