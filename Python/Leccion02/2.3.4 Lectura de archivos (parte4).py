be37c5985a7b35e587c4aa2de5198535069b4224
Jairo <jwu.monk@gmail.com>
2026-06-03 23:14:32 -0300

    2.3.4 Lectura de archivos (parte4)

---
+++ Python/Leccion02/2.3.4 Lectura de archivos (parte4).py
@@ -0,0 +1,12 @@
+# 2.3.4 Lectura de archivos (parte 4)
+
+archivo = open('prueba.txt', 'r', encoding='utf8') # Las letras son: 'r' read, 'a' append, 'w' write, 'x'
+# Si el archivo esta en otra carpeta, hay que colocar la ruta de la carpeta
+
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10)) # Continuamos desde la línea anterior
+#print(archivo.readline())
+#print(archivo.readline())
+
+print('=======')
