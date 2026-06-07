b455d452f2f8e7772718736a008af5d612eb2b94
Jairo <jwu.monk@gmail.com>
2026-06-07 20:46:25 -0300

    4.1 Bloque try catch y finally.js

--- JavaScript/Leccion04/4.1 Bloque try catch y finally.js
+++ JavaScript/Leccion04/4.1 Bloque try catch y finally.js
@@ -2,13 +2,11 @@
 
 let x = 10;
 
-
-
 //Veremos como evitar el error
 try {
-    let x = 10; 
+    let x = 10;
     miFuncion();
-
+    throw 'Mi error'; //Maneja tipo string
 }
 catch (error) { //catchamos el error
     console.log('Se ha producido un error: ' + error);
@@ -19,3 +17,21 @@ finally {
 
 
 console.log('Continuamos... '); //Esto no se llega a ver porque esta bloqueado (Roto)
+
+//
+let resultado = 'hola';
+try {
+    //y = 5;
+    if (isNaN(resultado)) throw 'no es un numero';
+    else if(resultado === '') throw 'Es una cadena vacia';
+    else if(resultado >= 0) throw 'Valor positivo';
+    else if(resultado < 0) throw 'Valor negativo';
+}
+catch (error) {
+    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
+}
+finally {
+    console.log('Fin de la revision de errores');
+}
