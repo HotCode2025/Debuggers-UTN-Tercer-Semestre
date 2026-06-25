ce6b58c12c87e1695801e71fa6378a7aa7df1992
Kevin Moratal <kevinjoelmoratal96@gmail.com>
2026-06-25 16:52:55 -0300

    Tareas pendientes python

---
+++ Python/Leccion08/9.2 Prueba metodo seleccionar.py
@@ -0,0 +1,34 @@
+from capa_datos_persona.Persona import Persona
+from capa_datos_persona.conexion import Conexion
+from logger_base import log
+class PersonaDAO:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+                    Create  -> Insertar
+                    Reade   -> Seleccionar
+                    Update  -> Actualizar
+                    Deleted -> Eliminar
+    """
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = [] #Creamos una lista
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+                return personas
+
+if __name__ == '__main__':
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
