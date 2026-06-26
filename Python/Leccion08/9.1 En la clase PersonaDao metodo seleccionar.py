11c5b20a7fc8e7de5da6d4c283d80d9c7555321a
EnzoNavarro36 <navarroenzo724@gmail.com>
2026-06-26 18:09:36 -0300

    9.1 En la clase PersonaDao Metodo Seleccionar

---
+++ Python/Leccion08/9.1 En la clase PersonaDao metodo seleccionar.py
@@ -0,0 +1,26 @@
+class PersonaDao:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+        create --> Insertar
+        Read --> Seleccionar
+        Update --> Actualizar
+        Delete --> Eliminar 
+    """
+
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre,apellido,email) VALUES (%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obetenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = [] #creamos una lista
+                for registro in registros:
+                    persona = Persona(registro[0],registro[1],registro[2],registro[3])
+                    personas.append(persona)
