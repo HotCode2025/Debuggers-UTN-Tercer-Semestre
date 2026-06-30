147865ad6077c9bd19816cd3b0b4b7dbd57ea900
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2026-06-30 20:46:46 -0300

    2.2 Manejo de Enumeraciones (enum)

---
+++ Java/Leccion02/2.2 Manejo de Enumeraciones (enum) - TestEnumeracioes.java
@@ -0,0 +1,9 @@
+package test;
+
+import enumeracion.Dias;
+
+public class TestEnumeracioes {
+    public static void main(String[] args) {
+        System.out.println("Día 1: " + Dias.LUNES);
+    }
+}
