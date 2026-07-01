afd2b056e86677863c9dee3ef1cad328e6f0deba
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2026-06-30 22:18:28 -0300

    2.2 Creamos un archivo de la clase de hoy

---
+++ JavaScript/Leccion02/2.2 Creamos un archivo de la clase de hoy.js
@@ -0,0 +1,14 @@
+"use strict"; // Con esta linea le decimos que no se pueden utilizar variables si no estan declaradas
+
+let x = 10;
+console.log(x)
+
+function miFuncion() {
+    "use strict"
+    let y = 10;
+    console.log(y);
+
+}
+
+miFuncion();
+
