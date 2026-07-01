9c5c7d18a1a0497881b81972e0f44a710ee4eef4
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2026-06-30 22:29:45 -0300

    10.3 Pruebas creando objetos del pool de conexiones

---
+++ Python/Leccion09/10.3 Pruebas creando objetos del pool de conexiones.py
@@ -0,0 +1,54 @@
+import sys
+import psycopg2 as bd
+from logger_base import log
+from psycopg2 import pool #Se importa 'pool' de psycopg2 para manejar el grupo de conexiones
+
+class Conexion:
+    # Atributos de configuración de la base de datos
+    _DATABASE: str = "test_bd"
+    _USERNAME: str = "postgres"
+    _PASSWORD: str = "admin"
+    _DB_PORT: str = "5432"
+    _HOST: str = "127.0.0.1"
+    # Parámetros del Pool: mínimo 1 conexión abierta, máximo 5 simultáneas
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None # Acá se guardará el objeto del Pool una vez creado
+
+    @classmethod
+    def obtenerConexion(cls):
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexion obtenida del pool: {conexion}')
+        return conexion
+    
+    @classmethod
+    def obtenerCursor(cls):
+        pass
+    @classmethod
+    def crearConexion(cls):
+        pass
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                  cls._MAX_CON,
+                                                  host=cls._HOST,
+                                                  user=cls._USERNAME,
+                                                  password=cls._PASSWORD,
+                                                  port=cls._DB_PORT,
+                                                  database=cls._DATABASE)
+                log.debug(f'creacion del pool exitosa: {cls._pool}')
+                return cls._pool
+            except Exception as e:
+                log.error(f'Ocurrio un error al obtener el pool: {e}')
+                sys.exit()
+        else:
+            return cls._pool
+if __name__ == "__main__":
+    conexion1 = Conexion.obtenerConexion()
+    conexion2 = Conexion.obtenerConexion()
+    conexion3 = Conexion.obtenerConexion()
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+
