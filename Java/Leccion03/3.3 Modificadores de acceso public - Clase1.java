bafc70a75d005a743d80b14f788742ea784022da
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2026-06-30 21:06:57 -0300

    3.3 Modificadores de acceso public

---
+++ Java/Leccion03/3.3 Modificadores de acceso public - Clase1.java
@@ -0,0 +1,13 @@
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublic = "Valor atributo publico";
+    
+    public Clase1() {
+        System.out.println("Constructor public");
+    }
+    
+    public void motrodPublico() {
+        System.out.println("Metodo publico");
+    }
+}
